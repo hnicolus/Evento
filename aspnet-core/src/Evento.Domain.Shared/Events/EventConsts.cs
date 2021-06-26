9b0a90fab7f085dd24e92b5b2f7e5f664f774b7b
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-06-26 12:34:28 +0200

    Added Event Constants

---
+++ aspnet-core/src/Evento.Domain.Shared/Events/EventConsts.cs
@@ -0,0 +1,9 @@
+namespace Evento.Events
+{
+    public static class EventConsts
+    {
+        public const int TitleMaxLength = 50;
+        public const int DescriptionMaxLength = 2000;
+        
+    }
+}
