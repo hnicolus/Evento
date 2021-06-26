20d55b0e098c1ddecec37b26b384acabe25677c9
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-06-26 12:33:12 +0200

    Added event Creation Dto

---
+++ aspnet-core/src/Evento.Application.Contracts/Events/EventCreationDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Evento.Events
+{
+    public class EventCreationDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        public string Description { get; set; }
+
+        public bool IsFree { get; set; }
+
+        public DateTime StartTime { get; set; }
+    }
+}
