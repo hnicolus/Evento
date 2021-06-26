f983a6f2b35c0352ba43dd5d7994787e886413d0
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-06-26 12:35:28 +0200

    Added events Page

---
+++ aspnet-core/src/Evento.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Evento.Events;
+using Microsoft.AspNetCore.Components;
+
+namespace Evento.Blazor.Pages
+{
+    public partial class Index
+    {
+        private IReadOnlyList<EventDto> UpcomingEvents { get; set; } = new List<EventDto>();
+        [Inject] private IEventAppService EventAppService { get; set; }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            UpcomingEvents = (await EventAppService.GetUpcomingAsync()).Items;
+        }
+    }
+}
