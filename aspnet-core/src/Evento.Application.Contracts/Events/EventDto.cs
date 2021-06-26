a212e70c4cd65d85c33d8a8b7545d382bf40849d
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-06-26 12:33:19 +0200

    Event Dto

---
+++ aspnet-core/src/Evento.Application.Contracts/Events/EventDto.cs
@@ -0,0 +1,18 @@
+using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Evento.Events
+{
+    public class EventDto : EntityDto<Guid>
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsFree { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public int AttendeesCount { get; set; }
+    }
+}
